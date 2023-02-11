935ea7bb43037dcfcc224cf8b2d1cf5583df4198
Hari Gopi Krishna Jorige <63456015+jorigeharigopikrishna@users.noreply.github.com>
2023-02-12 01:14:41 +0530

    single and multi-line in line plot chart.

---
+++ matplotlib_charts/line_plot_charts/weather_line_plot_chart.py
@@ -0,0 +1,29 @@
+import pandas as pandas_package
+import matplotlib.pyplot as plot_package
+
+df_object = pandas_package.read_csv("../../Datasets/bangalore_jan_weather_report.csv", parse_dates=["Date"])
+date_series = df_object["Date"]
+temperature_series = df_object["Temperature"]
+windspeed_series = df_object["Windspeed"]
+
+plot_package.title("Temperature in Bangalore in January month")     # Set title of chart
+plot_package.xlabel("Date")     # Set label for x-axis
+plot_package.ylabel("Temperature")      # Set label for y-axis
+plot_package.plot(date_series, temperature_series)  # Plot line graph between date and temperature.
+plot_package.show()     # Display the line plot chart
+
+# For single-line charts, label attribute is not required.
+# For multi-line charts, Setting label for line will be useful only for applying legend in line plot chart.
+plot_package.plot(date_series, temperature_series, label="temperature")
+
+plot_package.plot(date_series, temperature_series, alpha=0.3)  # Controls the transparency / brightness of the line / markers in graph.
+
+plot_package.plot(date_series, temperature_series, marker="+")  # Display data points in graph with + symbol
+
+plot_package.plot(date_series, temperature_series, color="green")   # Line color is green
+
+plot_package.plot(date_series, temperature_series, linewidth=6)     # Width of the line is 6
+
+plot_package.plot(date_series, temperature_series, linestyle="dashed")  # Style of line is dashed
+
+plot_package.plot(date_series, temperature_series, linestyle="dotted")  # Style of line is dotted
---
+++ matplotlib_charts/line_plot_charts/weather_multi_lines_plot_chart.py
@@ -0,0 +1,26 @@
+import pandas as pandas_package
+import matplotlib.pyplot as plot_package
+
+df_object = pandas_package.read_csv("../../Datasets/bangalore_jan_weather_report.csv", parse_dates=["Date"])
+date_series = df_object["Date"]
+temperature_series = df_object["Temperature"]
+windspeed_series = df_object["Windspeed"]
+aqi_series = df_object["AQI"]
+
+plot_package.title("Weather Report in Bangalore in January month")     # Set title of chart
+plot_package.xlabel("Date")     # Set label for x-axis
+plot_package.ylabel("Temperature")      # Set label for y-axis
+
+# When used same x-axis data for multiple plots, all those plots will be in same chart with different line graphs.
+# In such case, label attribute should be defined to each line plot to help legend to distinguish them.
+plot_package.plot(date_series, temperature_series, label="Temperature")  # Plot line graph between date and temperature.
+plot_package.plot(date_series, windspeed_series, label="Windspeed")  # Plot line graph between date and windspeed.
+plot_package.plot(date_series, aqi_series, label="AQI")  # Plot line graph between date and AQI.
+plot_package.legend()   # add legend only in case of multi-lines in single chart
+
+plot_package.legend(loc="upper right")  # sets legend location to be upper right
+
+plot_package.legend(fontsize="large")  # sets fontsize of the labels in the legend to be large
+
+plot_package.grid()     # adds grid lines as canvas in the background.
+plot_package.show()     # Display line plot chart.
